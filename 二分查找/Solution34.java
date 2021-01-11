7379ddb95c5f3fa91f9d23aee977afa330c0961c
whhsnbb <1144320036@qq.com>
2021-01-11 15:27:12 +0800

    commit.

---
+++ 二分查找/Solution34.java
@@ -0,0 +1,83 @@
+package 二分查找;
+//34. 在排序数组中查找元素的第一个和最后一个位置
+//给定一个按照升序排列的整数数组 nums，和一个目标值 target。找出给定目标值在数组中的开始位置和结束位置。
+//
+//如果数组中不存在目标值 target，返回 [-1, -1]。
+//
+//进阶：
+//
+//你可以设计并实现时间复杂度为 O(log n) 的算法解决此问题吗？
+//
+//
+//示例 1：
+//
+//输入：nums = [5,7,7,8,8,10], target = 8
+//输出：[3,4]
+//示例 2：
+//
+//输入：nums = [5,7,7,8,8,10], target = 6
+//输出：[-1,-1]
+//示例 3：
+//
+//输入：nums = [], target = 0
+//输出：[-1,-1]
+class Solution34 {
+    //二分查找法
+    public int[] searchRange(int[] nums, int target) {
+        int len = nums.length;
+        if (len == 0) {
+            return new int[]{-1, -1};
+        }
+
+        int firstPosition = findFirstPosition(nums, target);
+        if (firstPosition == -1) {
+            return new int[]{-1, -1};
+        }
+
+        int lastPosition = findLastPosition(nums, target);
+        return new int[]{firstPosition, lastPosition};
+    }
+
+    private int findFirstPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right) / 2;                       //找第一个数时是上取整
+            // 小于一定不是解
+            if (nums[mid] < target) {
+                // 下一轮搜索区间是 [mid + 1, right]
+                left = mid + 1;
+            } else if (nums[mid] == target) {
+                // 下一轮搜索区间是 [left, mid]
+                right = mid;
+            } else {
+                // nums[mid] > target，下一轮搜索区间是 [left, mid - 1]
+                right = mid - 1;
+            }
+        }
+
+        if (nums[left] == target) {
+            return left;
+        }
+        return -1;
+    }
+
+    private int findLastPosition(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        while (left < right) {
+            int mid = (left + right + 1) / 2;                   //找最后一个数时是下取整
+            if (nums[mid] > target) {
+                // 下一轮搜索区间是 [left, mid - 1]
+                right = mid - 1;
+            } else if (nums[mid] == target){
+                // 下一轮搜索区间是 [mid, right]
+                left = mid;
+            } else {
+                // nums[mid] < target，下一轮搜索区间是 [mid + 1, right]
+                left = mid + 1;
+            }
+        }
+        return left;
+    }
+}
