1ede03b7ba359d09665d43182cfda582b716322a
whhsnbb <1144320036@qq.com>
2021-03-03 16:48:56 +0800

    one

---
+++ 链表/Solution138.java
@@ -0,0 +1,61 @@
+package 链表;
+//138. 复制带随机指针的链表
+//给定一个链表，每个节点包含一个额外增加的随机指针，该指针可以指向链表中的任何节点或空节点。
+//
+//要求返回这个链表的 深拷贝。
+//
+//我们用一个由 n 个节点组成的链表来表示输入/输出中的链表。每个节点用一个 [val, random_index] 表示：
+//
+//val：一个表示 Node.val 的整数。
+//random_index：随机指针指向的节点索引（范围从 0 到 n-1）；如果不指向任何节点，则为  null 。
+
+/**
+ * 必须为复制的节点，原链表所有东西不能改变，也不能直接用原链表
+ */
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+
+public class Solution138 {
+    public Node copyRandomList(Node head) {
+        if(head==null) {
+            return null;
+        }
+        Node p = head;
+        //第一步，在每个原节点后面创建一个新节点
+        //1->1'->2->2'->3->3'
+        while(p!=null) {
+            Node newNode = new Node(p.val);
+            newNode.next = p.next;
+            p.next = newNode;
+            p = newNode.next;
+        }
+        p = head;
+        //第二步，设置新节点的随机节点
+        while(p!=null) {
+            if(p.random!=null) {
+                p.next.random = p.random.next;
+            }
+            p = p.next.next;
+        }
+        Node dummy = new Node(-1);
+        p = head;
+        Node cur = dummy;
+        //第三步，将两个链表分离
+        while(p!=null) {
+            cur.next = p.next;
+            cur = cur.next;
+            p.next = cur.next;
+            p = p.next;
+        }
+        return dummy.next;
+    }
+}
