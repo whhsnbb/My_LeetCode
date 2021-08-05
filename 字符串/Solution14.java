1626323fddbc82204942a912f6a398539d22957f
whhsnbb <1144320036@qq.com>
2021-08-05 13:15:00 +0800

    one commit.

---
+++ 字符串/Solution14.java
@@ -0,0 +1,33 @@
+package 字符串;
+//14. 最长公共前缀
+//编写一个函数来查找字符串数组中的最长公共前缀。
+//
+//如果不存在公共前缀，返回空字符串 ""。
+//
+//示例 1:
+//
+//输入: ["flower","flow","flight"]
+//输出: "fl"
+//示例 2:
+//
+//输入: ["dog","racecar","car"]
+//输出: ""
+//解释: 输入不存在公共前缀。
+public class Solution14 {
+    public String longestCommonPrefix(String[] strs) {
+        if(strs.length == 0)
+            return "";
+        String ans = strs[0];
+        for(int i =1;i<strs.length;i++) {
+            int j=0;
+            for(;j<ans.length() && j < strs[i].length();j++) {
+                if(ans.charAt(j) != strs[i].charAt(j))
+                    break;
+            }
+            ans = ans.substring(0, j);
+            if(ans.equals(""))
+                return ans;
+        }
+        return ans;
+    }
+}
